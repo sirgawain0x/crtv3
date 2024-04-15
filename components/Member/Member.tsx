d8bca0879b603f8b238c216afd856e0de83c6cea
Gawain Bracy II <gawainbracyii@Gawains-MacBook-Pro.local>
2024-04-15 15:53:50 -0400

    added pages
    - working on tokengating membership pages.

--- components/Member/Member.tsx
+++ components/Member/Member.tsx
@@ -5,6 +5,12 @@ import {
   Link,
   Text,
   VStack,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
 } from '@chakra-ui/react';
 import { TokenGateContext } from 'collabland-tokengate-react-context';
 import { useFormik } from 'formik';
@@ -36,6 +42,7 @@ const RulesValidationSchema = Yup.object().shape({
 const Member = () => {
     const activeAccount = useActiveAccount();
     const { checkRoles, isLoading, result, error } = useContext(TokenGateContext);
+    const { isOpen, onOpen, onClose } = useDisclosure();
     const formik = useFormik({
         initialValues: {
           address: activeAccount?.address,
@@ -200,17 +207,10 @@ const Member = () => {
                 </Link>
                 <Link
                   className="underline"
-                  href="https://creativeplatform.xyz/docs/intro"
-                  target="_blank"
-                >
-                  Creative Docs
-                </Link>
-                <Link
-                  className="underline"
                   href="https://creativeplatform.xyz/blog"
                   target="_blank"
                 >
-                  Purchase Membership Pass
+                  Membership Pricing
                 </Link>
               </VStack>
               {result?.roles?.[0] && (
@@ -244,6 +244,27 @@ const Member = () => {
                       4
                     )}
                   </pre>
+                  { result.roles[0].granted ? 
+                    <Button onClick={onOpen}>
+                    Creative TV Menu
+                  </Button> : <Link
+                      href="https://app.unlock-protocol.com/checkout?id=cac7160c-260b-467b-9012-0864039df0e3"
+                      target="_blank"
+                    >
+                      <Button my={4}>Purchase Membership Pass</Button>
+                    </Link>
+                  }
+                  <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
+                    <DrawerOverlay />
+                    <DrawerContent>
+                      <DrawerHeader borderBottomWidth='1px'>Creative TV Menu</DrawerHeader>
+                      <DrawerBody>
+                        <Link href={`/profile/${activeAccount?.address}`}>My Profile</Link>
+                        <p>Some contents...</p>
+                        <p>Some contents...</p>
+                      </DrawerBody>
+                    </DrawerContent>
+                  </Drawer>
                 </div>
               )}
             </VStack>
---
+++ src/app/discover/[id]/page.tsx
@@ -0,0 +1,83 @@
+import React, { useMemo } from 'react'
+import { useRouter } from 'next/router'
+import { Metadata } from 'next'
+import { Player, useAsset } from '@livepeer/react'
+import { CREATIVE_LOGO_WHT } from '../../../../utils/context'
+import {
+    Box,
+    Button,
+    Container,
+    Image,
+} from '@chakra-ui/react'
+
+const PosterImage = () => {
+    return <Image src={`${CREATIVE_LOGO_WHT}`} objectFit="contain" sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="Creative Logo" />
+}
+
+export const metadata: Metadata = {
+    title: "Creative TV",
+}
+
+const VideoPage = () => {
+    const router = useRouter()
+    // Extract the assetId from the router's query parameters
+    const assetId = useMemo(() => (router?.query?.assetId ? String(router?.query?.assetId) : undefined), [router?.query])
+
+    // Fetch the asset data using the useAsset hook
+    const { data: asset, isLoading: assetLoading, isError: assetError } = useAsset({
+        assetId,
+        enabled: assetId?.length === 36,
+        refetchInterval: (asset) => (asset?.storage?.status?.phase !== 'ready' ? 5000 : false),
+    })
+    // Check if the asset data is still loading
+    if (assetLoading) {
+        // Render loading state
+        return <Box>Loading asset data...</Box>
+    }
+    
+    // Check if there was an error fetching the asset data
+    if (assetError) {
+        // Render error state
+        return <Box>Error fetching asset data</Box>
+    }
+
+    return (
+        <>
+            <Container maxW={"1200px"} mt={10}>
+                <Button colorScheme={'blue'} onClick={() => router.push("/discover")}>Back</Button>
+                <Box mt={10}>
+                    <Player
+                        title={asset?.name}
+                        playbackId={asset?.playbackId}
+                        showTitle
+                        poster={<PosterImage />}
+                        showLoadingSpinner
+                        controls={{ autohide: 500, hotkeys: false }}
+                        aspectRatio="16to9"
+                        showPipButton
+                        autoUrlUpload={{ fallback: true, ipfsGateway: 'https://w3s.link' }}
+                        theme={{
+                            borderStyles: {
+                            containerBorderStyle: 'solid',
+                            },
+                            colors: {
+                            accent: '#EC407A',
+                            },
+                            space: {
+                            controlsBottomMarginX: '10px',
+                            controlsBottomMarginY: '5px',
+                            controlsTopMarginX: '15px',
+                            controlsTopMarginY: '10px',
+                            },
+                            radii: {
+                            containerBorderRadius: '0px',
+                            },
+                        }}
+                     />
+                </Box>
+            </Container>
+        </>
+    )
+}
+
+export default VideoPage;
