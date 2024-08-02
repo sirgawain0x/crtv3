82af4ac744e288898bdd5e41036562a00db5c182
Gawain Bracy II <gawainbracyii@gawains-mbp.lan>
2024-08-01 23:32:50 -0400

    playback update

--- app/components/home-page/TopVideos.tsx
+++ app/components/home-page/TopVideos.tsx
@@ -11,22 +11,30 @@ import {
 
 export function TopVideos() {
   return (
-    <Carousel className="mx-auto w-full max-w-lg">
-      <CarouselContent className="-ml-1">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/2">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-2xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="mx-auto w-full max-w-6xl py-8">
+      <div className="mb-8 text-center">
+        <h1 className="mb-4 text-3xl font-bold">Trending Videos</h1>
+      </div>
+      <Carousel className="min-w-sm mx-auto w-full max-w-lg">
+        <CarouselContent className="-ml-1">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <CarouselItem
+              key={index}
+              className="pl-1 md:basis-1/2 lg:basis-1/2"
+            >
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-2xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
 }
--- app/lib/sdk/livepeer/client.ts
+++ app/lib/sdk/livepeer/client.ts
@@ -1,5 +1,13 @@
 import { Livepeer } from 'livepeer';
+import { getSrc } from '@livepeer/react/external';
 
-export const livepeer = new Livepeer({
+const livepeer = new Livepeer({
   apiKey: `Bearer ${process.env.LIVEPEER_API_KEY}`,
 });
+
+export async function getSourceForPlaybackId(playbackId: string) {
+  const response = await livepeer.playback.get(playbackId);
+
+  // the return value can be passed directly to the Player as `src`
+  return getSrc(response.playbackInfo);
+}
