e368c7dc1b6ddee6e1966640fda8f80bfe2d3d53
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-12-01 14:08:25 +0100

    test: add titleCase

---
+++ app/lib/helpers/__tests__/titleCase.test.ts
@@ -0,0 +1,12 @@
+import { titleCase } from '../index';
+
+describe('parseTimestampToDate', () => {
+  it('should throw on empty string', () => {
+    expect(() => titleCase('')).toThrow('Can not parse empty string');
+  });
+
+  it('should return first letter capitalised', () => {
+    const res = titleCase('not over yet');
+    expect(res).toBe('Not over yet');
+  });
+});
