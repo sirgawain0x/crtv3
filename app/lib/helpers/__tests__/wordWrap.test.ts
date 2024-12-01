3010673c32b738e5eef658c3f8d8d29adfe9d879
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-12-01 13:58:18 +0100

    test: add wordWrap

---
+++ app/lib/helpers/__tests__/wordWrap.test.ts
@@ -0,0 +1,20 @@
+import { wordWrap } from '../index';
+
+describe('parseTimestampToDate', () => {
+  let txt = '';
+  beforeEach(() => {
+    txt = 'The function wrap a sentence at particular length of characters';
+  });
+  afterEach(() => (txt = ''));
+
+  it('should throw error if text lenght is shorter', () => {
+    expect(() => wordWrap(txt, 102)).toThrow(
+      `String lenght of ${txt.length} is shorter than wrapAfter of 102`,
+    );
+  });
+
+  it('should return a substring of text', () => {
+    const res = wordWrap(txt, 12);
+    expect(res).toBe(res.substring(0, 12) + '...');
+  });
+});
