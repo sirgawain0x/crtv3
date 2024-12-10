d0f0964d89168136164be9dc0d3516fe87ec6ff5
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-12-10 23:28:41 +0100

    update: add filteredCreatorAssets body

--- app/components/list-uploaded-assets/ListUploadedAssets.tsx
+++ app/components/list-uploaded-assets/ListUploadedAssets.tsx
@@ -1,7 +1,10 @@
 import { fetchAllAssets } from '@app/api/livepeer/actions';
+import useLazyMint from '@app/hooks/useLazyMint';
+import * as helpers from '@app/lib/helpers';
 import { Asset } from '@app/lib/types';
-import { Box, Skeleton } from '@chakra-ui/react';
+import { Box, Link, Spinner } from '@chakra-ui/react';
 import { useEffect, useMemo, useState } from 'react';
+import Modal from '../Modal';
 
 type ListUploadedAssetsProps = {
   [index: string]: any;
@@ -29,17 +32,16 @@ export default function ListUploadedAssets(props: ListUploadedAssetsProps) {
       });
   }, [props]);
 
-  const filteredAssets: Asset[] = useMemo(() => {
+  const filteredCreatorAssets: Asset[] = useMemo(() => {
     return Array.isArray(assets)
       ? assets.filter(
-          (ast: Asset) => ast.creatorId?.value === props.activeAccount.addresss,
+          (ast: Asset) =>
+            ast.creatorId &&
+            ast.creatorId.value.toLowerCase() ==
+              props.activeAccount.address.toLowerCase(),
         )
       : [];
-  }, [assets, props.activeAccount.addresss]);
-
-  if (isLoading) {
-    return <Skeleton height={'20px'} />;
-  }
+  }, [assets, props.activeAccount.address]);
 
   if (error) {
     return (
@@ -49,5 +51,137 @@ export default function ListUploadedAssets(props: ListUploadedAssetsProps) {
     );
   }
 
-  return <></>;
+  return (
+    <div>
+      {isLoading && <Spinner />}
+
+      {filteredCreatorAssets.length > 0 && (
+        <>
+          <h1 style={{ margin: '24px 0', fontSize: '18px' }}>
+            List of uploaded videos
+          </h1>
+          <table className="w-full table-auto">
+            <thead>
+              <tr className="text-sm text-gray-600">
+                <th className="border border-slate-600 px-4 py-1 ">Name</th>
+                <th className="border border-slate-600 px-4 py-1">Created</th>
+                <th className="border border-slate-600 px-4 py-1">Updated</th>
+                <th className="border border-slate-600 px-4 py-1">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredCreatorAssets.map((video, i) => (
+                <tr key={i}>
+                  <td className="border border-slate-700 px-4 py-1">
+                    <Link
+                      href={`${props.activeAccount.addresss}/${video.id}?video=${JSON.stringify(video)}`}
+                    >
+                      {helpers.titleCase(
+                        video?.name.length > 12
+                          ? video?.name.slice(0, 9) + '...'
+                          : video?.name,
+                      )}
+                    </Link>
+                  </td>
+                  <td className="border border-slate-700 px-4 py-1">
+                    {helpers.parseTimestampToDate(video?.createdAt as any)}
+                  </td>
+                  <td className="border border-slate-700 px-4 py-1">
+                    {helpers.parseTimestampToDate(
+                      video?.status?.updatedAt as any,
+                    )}
+                  </td>
+
+                  <td className="border border-slate-700 px-4 py-1">
+                    {/* TODO: Need to check how to fix this with
+                      1. Fetch lazyMinted 
+                      2. Filter its data against `filteredCreatorAssets`
+                    */}
+                    {video.storage && video.storage.ipfs.nftMetadata ? (
+                      <Modal
+                        title="Lazy Mint"
+                        triggerText="Need Minting"
+                        body={
+                          <Body
+                            baseURIForToken={
+                              video.storage &&
+                              String(video.storage.ipfs.nftMetadata.gatewayUrl)
+                            }
+                          />
+                        }
+                      />
+                    ) : (
+                      'Minted'
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="modal-box"></div>
+        </>
+      )}
+    </div>
+  );
+}
+
+type BodyProps = {
+  baseURIForToken: string;
+};
+
+function Body(props: BodyProps) {
+  const { lazyMint } = useLazyMint();
+  const [formData, setFormData] = useState<Record<string, string>>({});
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    setFormData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleLazyMinting = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    lazyMint(formData.amount, props.baseURIForToken, {
+      price: formData.price,
+    });
+  };
+
+  return (
+    <>
+      <form>
+        <div>
+          <label htmlFor="amount">
+            <input
+              type="text"
+              name="amount"
+              id="amount"
+              placeholder="Amount to mint"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="price">
+            <input
+              type="text"
+              name="price"
+              id="price"
+              placeholder="Price of NFT"
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+
+        <button onClick={handleLazyMinting}>
+          {status === 'pending' ? 'Minting' : 'Mint'}
+        </button>
+      </form>
+    </>
+  );
 }
