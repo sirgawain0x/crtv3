bed29b457db3c466dacfb82797529ec6a1f296d2
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-12-29 12:11:59 +0100

    update: add ClaimVideoNFT comp

--- app/components/claim-vidoe-nft/ClaimVideoNFT.tsx
+++ app/components/claim-vidoe-nft/ClaimVideoNFT.tsx
@@ -1,3 +1,49 @@
-import { CONTRACT_ADDRESS } from '@app/lib/utils/context';
-import { useContract } from '@thirdweb-dev/react';
+import { useState } from 'react';
+import { ContractOptions } from 'thirdweb';
+import { useActiveAccount } from 'thirdweb/react';
+import { NFT, ResolvedReturnType } from '@app/types/nft';
 
+type ClaimVideoNFTProps = {
+  videoContract: Readonly<ContractOptions<[]>> | undefined;
+};
+
+export default function ClaimVideoNFT(props: ClaimVideoNFTProps) {
+  const activeAccount = useActiveAccount();
+  const [qty, setQTY] = useState(1);
+
+  const handleSubmit = async () => {
+
+    console.log({ qty });
+
+
+    return;
+    // const transaction = claimTo({
+    //   contract: props.videoContract,
+    //   to: String(activeAccount?.address),
+    //   tokenId: 0n,
+    //   quantity: BigInt(qty),
+    // });
+
+    // await sendTransaction({ transaction, account: activeAccount!! });
+  };
+
+  const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setQTY(Number(e.currentTarget.value));
+  };
+
+  return (
+    <div>
+      <h4 className="mb-1 text-lg text-slate-300">ClaimNFT For Creator</h4>
+      <p className="mb-2 text-sm text-slate-400">
+        This hold the claim component for the creator
+      </p>
+
+      <form className="flex flex-col gap-4">
+        <input type="number" name="quantity" onChange={handleOnChange} min={qty}  />
+        <input type="submit" name="submit" onClick={handleSubmit} />
+      </form>
+    </div>
+  );
+}
