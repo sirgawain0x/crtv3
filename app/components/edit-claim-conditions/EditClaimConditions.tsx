a6293be4d1877bd1d17265e04cdce459d0f40f4e
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2024-12-29 12:09:12 +0100

    fix: attempt to use cc update from old thirdweb sdk

--- app/components/edit-claim-conditions/EditClaimConditions.tsx
+++ app/components/edit-claim-conditions/EditClaimConditions.tsx
@@ -13,7 +13,9 @@ import {
   Select,
   useToast,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { CONTRACT_ADDRESS } from '@app/lib/utils/context';
+import { useContract } from '@thirdweb-dev/react';
+import { useState } from 'react';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { ContractOptions } from 'thirdweb';
 import { getClaimConditions } from 'thirdweb/extensions/erc1155';
@@ -24,7 +26,6 @@ type EditClaimFormData = {
   maxClaimablePerWallet: string;
   maxClaimableSupply?: string;
   startTimestamp: string;
-  // waitInSeconds: string;
 };
 
 type EditClaimConditionsProps = {
@@ -38,8 +39,11 @@ type EditClaimConditionsProps = {
 export default function EditClaimConditions(props: EditClaimConditionsProps) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isErrorFree, setIsErrorFree] = useState(false);
-  const [isExecuted, setIsExecuted] = useState(false); // to clear form data
 
+  // TODO: Revisit to remove old skd usage
+  const { contract: nftContract } = useContract(
+    CONTRACT_ADDRESS.editionDrop.erc1155.amoy,
+  );
   const toast = useToast();
   const cc = props.claimConditions[props.ccIndex];
 
@@ -49,22 +53,18 @@ export default function EditClaimConditions(props: EditClaimConditionsProps) {
     formData: EditClaimFormData,
   ): Promise<boolean | undefined> => {
     // update an existing claimCondition by its id
+    console.log({ ccIndex, tokenId, formData });
 
     try {
-      // await props.nftContract?.erc1155.claimConditions.update(
-      //   tokenId,
-      //   ccIndex,
-      //   {
-      //     startTime: formData.startTimestamp, // When the phase starts (i.e. when users can start claiming tokens)
-      //     maxClaimableSupply: formData.maxClaimableSupply, // limit how many mints for this presale
-      //     currency: formData.currency, // The address of the currency you want users to pay in
-      //     maxClaimablePerWallet: formData.maxClaimablePerWallet, // The maximum number of tokens a wallet can claim
-      //     metadata: {
-      //       name: formData.phaseName, // Name of the sale's phase
-      //     },
-      //     // waitInSeconds: formData.waitInSeconds, // How long a buyer waits before another purchase is possible
-      //   },
-      // );
+      await nftContract?.erc1155.claimConditions.update(tokenId, ccIndex, {
+        startTime: Number(formData.startTimestamp), // When the phase starts (i.e. when users can start claiming tokens)
+        maxClaimableSupply: formData.maxClaimableSupply, // limit how many mints for this presale
+        currencyAddress: formData.currency, // The address of the currency you want users to pay in
+        maxClaimablePerWallet: formData.maxClaimablePerWallet, // The maximum number of tokens a wallet can claim
+        metadata: {
+          name: formData.phaseName, // Name of the sale's phase
+        },
+      });
 
       return true;
     } catch (err) {
@@ -83,14 +83,11 @@ export default function EditClaimConditions(props: EditClaimConditionsProps) {
   const sumbitUpdatedCC: SubmitHandler<EditClaimFormData> = async (data) => {
     const { errors } = formState;
 
-    console.log('form submitting...');
-
     const isRequiredFields =
       errors.maxClaimablePerWallet?.type === 'required' ||
       errors.currency?.type === 'required' ||
       errors.phaseName?.type === 'required' ||
       errors.startTimestamp?.type === 'required';
-    // || errors.waitInSeconds?.type === 'required';
 
     if (isRequiredFields) {
       return;
@@ -98,24 +95,19 @@ export default function EditClaimConditions(props: EditClaimConditionsProps) {
 
     const dateObj = new Date(data.startTimestamp);
 
-    const formData = {
-      ...data,
-      maxClaimablePerWallet: data.maxClaimablePerWallet,
-      // waitInSeconds: data.waitInSeconds,
-      startTimestamp: dateObj.getTime(),
-    };
-
     try {
       setIsErrorFree(true);
       setIsSubmitting(true);
 
-      // TODO: remove after debugging
-      console.log({ formData });
-
-      // await handleUpdateClaimCondition(props.nft.id, props.ccIndex, formData);
+      await handleUpdateClaimCondition(props.nft.id, props.ccIndex, {
+        ...data,
+        maxClaimablePerWallet: data.maxClaimablePerWallet,
+        startTimestamp: dateObj.getTime() as unknown as string,
+      });
     } catch (err: any) {
       setIsSubmitting(false);
       console.error(err);
+
       toast({
         title: 'Set Claim Conditions',
         description: `Setting claim conditions failed!`,
@@ -142,7 +134,8 @@ export default function EditClaimConditions(props: EditClaimConditionsProps) {
           defaultValue={''}
         >
           <div className="my-6 flex flex-col gap-2">
-            {/* <div alignItems={'flex-start'}>
+            {/* TODO: Try figure how this field would be useful */}
+            <div className="flex flex-col gap-1">
               <FormLabel>Name of Phase</FormLabel>
               <Controller
                 name="phaseName"
@@ -150,23 +143,26 @@ export default function EditClaimConditions(props: EditClaimConditionsProps) {
                 rules={{ required: true }}
                 render={({ field }) => (
                   <Input
-                    color={'gray.300'}
-                    defaultValue={cc.metadata?.name || ''}
-                    size={'lg'}
+                    {...field}
                     {...register('phaseName')}
+                    color={'gray.300'}
+                    value={
+                      field.value || field.name + '-' + new Date().getTime()
+                    }
+                    onChange={(e) => field.onChange(e.target.value)}
+                    size={'lg'}
                     mb={formState.errors.phaseName ? 0 : 4}
                     placeholder="Enter name to for this phase (Phase 1)"
                     aria-invalid={formState.errors.phaseName ? 'true' : 'false'}
-                    value={field.value}
                   />
                 )}
               />
               {formState.errors.phaseName?.type === 'required' && (
-                <FormHelperText className="text-red-500 mb-8">
+                <FormHelperText className="mb-8 text-red-500">
                   Select a name for the phase of sales.
                 </FormHelperText>
               )}
-            </div> */}
+            </div>
             <div className="flex flex-col gap-1">
               <FormLabel>Start time of Phase</FormLabel>
               <Controller
