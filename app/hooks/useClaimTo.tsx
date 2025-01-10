228d019465a36f29c22e83c35a80acb8038add26
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2025-01-11 00:08:30 +0100

    fix: Add txn timeout

--- app/hooks/useClaimTo.tsx
+++ app/hooks/useClaimTo.tsx
@@ -1,5 +1,6 @@
 // import { videoContract } from '@app/lib/sdk/thirdweb/get-contract';
 import type { TVideoContract } from '@app/types/nft';
+import { reject } from 'lodash';
 import { useState } from 'react';
 import { sendTransaction } from 'thirdweb';
 import { claimTo } from 'thirdweb/extensions/erc1155';
@@ -26,6 +27,8 @@ function useClaimTo(props: TClaimProps) {
   const [isProcessing, setIsProcessing] = useState(false);
 
   const handleClaim = async (args: IClaimToArgs) => {
+    const TRANSACTION_TIMEOUT = 30000;
+
     (Object.keys(args) as (keyof IClaimToArgs)[]).forEach((key) => {
       if (!args[key]) {
         throw new Error(`${key} is required`);
@@ -34,20 +37,34 @@ function useClaimTo(props: TClaimProps) {
 
     setIsProcessing(true);
 
-    try {
-      console.log({ args });
+    if (!props.videoContract) {
+      throw new Error('NFT contract is undefined!');
+    }
 
+    try {
       const transaction = claimTo({
-        contract: props.videoContract!,
+        contract: props.videoContract,
         quantity: BigInt(args.quantity),
         to: args.to,
         tokenId: BigInt(args.tokenId),
       });
 
-      const { transactionHash } = await sendTransaction({
-        transaction,
-        account: activeAccount!,
+      const timeoutPromise = new Promise((_, reject) => {
+        setTimeout(
+          () => reject(new Error('Transaction timeout')),
+          TRANSACTION_TIMEOUT,
+        );
       });
+
+      const result = await Promise.race([
+        sendTransaction({
+          transaction,
+          account: activeAccount!,
+        }),
+        timeoutPromise,
+      ]) as { transactionHash: string };
+
+      const { transactionHash } = result;
 
       if (transactionHash) {
         return transactionHash;
@@ -60,7 +77,7 @@ function useClaimTo(props: TClaimProps) {
         message: (err as Error).message,
         code: (err as IClaimToError).code,
       };
-      
+
       setError(claimToError);
     } finally {
       setIsProcessing(false);
