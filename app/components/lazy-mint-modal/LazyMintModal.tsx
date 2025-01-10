bb1c2767e64fdbff7a28d8067d2427f1d1f0a917
Ogheneochuko <7204868+amochuko@users.noreply.github.com>
2025-01-10 16:59:56 +0100

    fix: Add accessibility

--- app/components/lazy-mint-modal/LazyMintModal.tsx
+++ app/components/lazy-mint-modal/LazyMintModal.tsx
@@ -1,5 +1,5 @@
-import LazyMintForm from '../forms/LazyMintForm';
- 
+import LazyMintForm from '../lazy-mint/LazyMint';
+
 type LazyMintProps = {
   baseURIForToken: string;
   toggleModal: () => void;
@@ -10,10 +10,12 @@ export default function LazyMintModal(props: LazyMintProps) {
     <div className="fixed inset-0 h-screen overflow-y-auto bg-black bg-opacity-90">
       <div className="p relative top-96 mx-auto w-full max-w-md rounded-lg bg-white p-8 shadow dark:bg-slate-800">
         <button
+          type="button"
+          aria-label="Close modal"
           onClick={props.toggleModal}
           className="absolute right-4 top-2 text-gray-600 hover:text-gray-800 focus:outline-none dark:hover:text-gray-200"
         >
-          <p className="text-lg font-semibold">&times;</p>
+          <span className="text-lg font-semibold">&times;</span>
         </button>
 
         <LazyMintForm
