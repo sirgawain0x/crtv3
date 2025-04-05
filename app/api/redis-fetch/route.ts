0e5b805d4942b78a1590178bd2b85f035ea24ed7
sirgawain0x <bracy.gawain2014@gmail.com>
2025-04-05 15:54:43 -0400

    redis fix

---
+++ app/api/redis-fetch/route.ts
@@ -0,0 +1,24 @@
+import { Redis } from '@upstash/redis';
+import { NextResponse } from 'next/server';
+
+// Initialize Redis
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_KV_URL,
+  token: process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN,
+});
+
+export const POST = async () => {
+  try {
+    // Fetch data from Redis
+    const result = await redis.get('item');
+
+    // Return the result in the response
+    return new NextResponse(JSON.stringify({ result }), { status: 200 });
+  } catch (error) {
+    // Handle errors gracefully
+    console.error('Error fetching data from Redis:', error);
+    return new NextResponse(JSON.stringify({ error: 'Failed to fetch data' }), {
+      status: 500,
+    });
+  }
+};
