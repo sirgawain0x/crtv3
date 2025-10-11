afa1ffd7627fc2aea6801971fcb38d5904cfc52a
sirgawain0x <bracy.gawain2014@gmail.com>
2025-10-11 06:39:46 -0400

    bugfix

--- components/Videos/Upload/FileUpload.tsx
+++ components/Videos/Upload/FileUpload.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { toast } from "sonner";
 import { CopyIcon } from "lucide-react";
 import {
